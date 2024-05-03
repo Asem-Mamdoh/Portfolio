15588b3c060e64ca987e07043d11db7d857f2aae
elza3im1 <asoom.mamdoh@gmail.com>
2024-05-03 19:47:07 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// var typed = new Typed('.text',{
+//     Strings: ["Frontend Developer" , "Photographer" , "Web Developer"],
+//     typespeed: 50,
+//     backspeed: 10,
+    // backdelay: 100000,
+//     loop: true,
+// });
+
+
+// var typed = new Typed('.element', {
+//     strings: ["FRONT-END", "WEB DEVELEBER."],
+//     typeSpeed: 30
+//   });
+
+var typed = new Typed('.type', {
+    strings: ['Frontend Developer', 'Web Developer', 'Photographer' ],
+    typeSpeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true,
+  });
